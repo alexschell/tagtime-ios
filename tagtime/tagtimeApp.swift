e9fc3b072040ca4a81d438b29d632729790f9d95
alexschell <schell.alex@gmail.com>
2023-03-12 22:16:09 -0700

    Initial Commit

---
+++ tagtime/tagtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tagtimeApp.swift
+//  tagtime
+//
+//  Created by Alexander Schell on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct tagtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
